f91f7bb1f8f36a5bebcba85a35392482f6504dc6
Aswathi <aswathim2402@gmail.com>
2025-01-20 22:16:39 +0530

    Implemented responsive Navbar component with with links and sidebar menu.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
-
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import PlaceOrder from './pages/PlaceOrder'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar></Navbar>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/place-order' element={<PlaceOrder/>}></Route>
+        <Route path='/orders' element={<Orders/>}></Route>
+       
 
       </Routes>
     </div>
