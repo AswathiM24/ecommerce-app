dca70a3c5db230242bfc82e855da2661be348cd2
Aswathi <aswathim2402@gmail.com>
2025-02-13 23:48:09 +0530

    Admin panel to  add items

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
+import axios from 'axios'
+import {backendUrl} from '../App'
 
 const Add = () => {
 
@@ -16,10 +18,42 @@ const Add = () => {
   const [bestseller,setBestseller] = useState(false);
   const [sizes, setSizes] = useState([]);
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const formData = new FormData()
+
+      formData.append("name",name)
+      formData.append("description",description)
+      formData.append("price",price)
+      formData.append("category",category)
+      formData.append("subCategory",subCategory)
+      formData.append("bestseller",bestseller)
+      formData.append("sizes",JSON.stringify(sizes))
+
+      image1 && formData.append("image1",image1)
+      image2 && formData.append("image2",image2)
+      image3 && formData.append("image3",image3)
+      image4 && formData.append("image4",image4)
+
+      const response = await axios.post(backendUrl + "/api/product/add",formData)
+      console.log(response.data);
+
+
+
+      
+    } catch (error) {
+      
+    }
+
+  }
+
 
 
   return (
-    <form className='flex flex-col w-full items-start gap-3'>
+    <form onSubmit={onSubmitHandler} className='flex flex-col w-full items-start gap-3'>
       <div>
         <p className='mb-2'>Upload Image</p>
 
@@ -82,25 +116,25 @@ const Add = () => {
         <p className='mb-2'>Product Sizes</p>
         <div className='flex gap-3'>
           <div onClick={()=>setSizes(prev => prev.includes("S") ? prev.filter(item => item!== "S" ):[...prev])}>
-            <p className={`bg-slate-200 px-3 py-1 cursor-pointer`}>S</p>
+            <p className={`${sizes.includes("S") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>S</p>
           </div>
           <div onClick={()=>setSizes(prev => prev.includes("M") ? prev.filter(item => item!== "M" ):[...prev])}>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+            <p className={`${sizes.includes("M") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>M</p>
           </div>
           <div onClick={()=>setSizes(prev => prev.includes("L") ? prev.filter(item => item!== "L" ):[...prev])}>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+            <p className={`${sizes.includes("L") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>L</p>
           </div>
           <div onClick={()=>setSizes(prev => prev.includes("XL") ? prev.filter(item => item!== "XL" ):[...prev])}>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+            <p className={`${sizes.includes("XL") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>XL</p>
           </div>
           <div onClick={()=>setSizes(prev => prev.includes("XXL") ? prev.filter(item => item!== "XXL" ):[...prev])}>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+            <p className={`${sizes.includes("XXL") ? "bg-pink-100" : "bg-slate-200"} px-3 py-1 cursor-pointer`}>XXL</p>
           </div>
         </div>
       </div>
 
       <div className='flex gap-2 mt-2'>
-        <input type="checkbox" id="bestseller" />
+        <input onChange={()=>setBestseller(prev => !prev)} checked={bestseller} type="checkbox" id="bestseller" />
         <label className='cursor-pointer' htmlFor="bestseller">Add to bestseller</label>
       </div>
 
