da1d4dfb205368119f152ffcce589000e416b393
Aswathi <aswathim2402@gmail.com>
2025-03-11 12:24:31 +0530

    Format

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,18 @@
+
+
+//add products to user cart
+const addToCart = async (req,res) => {
+
+}
+
+//update user cart
+const updateCart = async (req,res) => {
+    
+}
+
+//add products to user cart
+const getUserCart = async (req,res) => {
+    
+}
+
+export {addToCart, updateCart, getUserCart}
